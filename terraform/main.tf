1a243bd56921c5d99901322865c0928dc138f6fd
Hannah Vivian Shaw <hvivianshaw@gmail.com>
2023-10-16 01:51:58 -0400

    remove token auth TODO- it's done

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,6 @@ terraform {
 
 provider "proxmox" {
   # Authentication
-  # TODO: Swap to token auth!
   pm_api_url          = var.pm_api_url
   pm_api_token_id     = var.pm_api_token_id
   pm_api_token_secret = var.pm_api_token_secret
